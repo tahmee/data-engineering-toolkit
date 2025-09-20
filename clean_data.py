977985cea721b553b4db6d1c8809de4fe57210ec
tahmee <susantamunokubie@gmail.com>
2025-09-20 23:01:15 +0100

    add data cleaning function

--- clean_data.py
+++ clean_data.py
@@ -1,2 +1,16 @@
 # Import pandas library
 import pandas as pd
+
+# Basic function that cleans data
+def clean_data(df):
+    """
+    This is a basic function, that takes a dataframe as argument and perform some cleaning functions.
+    """
+
+    # Drop duplicate rows
+    df = df.drop_duplicates()
+
+    # Strip whitespace from column names and standardise column names
+    df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+
+    return df 
