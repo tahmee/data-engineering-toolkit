5f0a4ee67c1d93163dcecb574c4beffcdf21b351
tahmee <susantamunokubie@gmail.com>
2025-09-20 23:30:34 +0100

    add load_to_csv function

--- load_data.py
+++ load_data.py
@@ -1,2 +1,12 @@
 # Import pandas library
 import pandas as pd
+
+# This script contains two data loading functions
+
+# Function one: Load data to csv file
+def load_to_csv(df, filename ="output.csv"):
+    """
+    This function takes the cleaned and transformed dataframe and stores it as a csv file
+    """
+    df.to_csv(filename, index = False)
+    print(f"Data saved to {filename}")
