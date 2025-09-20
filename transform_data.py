2957b0d9ed1480648298896e858c31d23868789c
tahmee <susantamunokubie@gmail.com>
2025-09-20 23:15:44 +0100

    add transform data function

--- transform_data.py
+++ transform_data.py
@@ -1,2 +1,17 @@
 # Import pandas as pd
 import pandas as pd
+
+# Basic data transformation function
+def transform_data(df):
+    """
+    This is a basic data transformation function that takes in a dataframe as input and 
+    returns a transformed dataframe
+    """
+
+    # Normalizing numeric columns
+    num_cols = df.select_dtypes(include=["number"]).columns
+    for col in num_cols:
+        if df[col].max() != df[col].min():  
+            df[col] = (df[col] - df[col].min()) / (df[col].max() - df[col].min())
+
+    return df
